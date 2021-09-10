919accb87b0e261b035718a38845dafedc3d8362
Rick <ricksmycodeguy@gmail.com>
2021-09-10 10:16:35 -0500

    Built out index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
 import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducers";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 const { worker } = require('./mocks/browser');
 worker.start();
@@ -10,7 +16,9 @@ worker.start();
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
     rootElement
 );
 
